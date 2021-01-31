1b54124afd78682904f0002672f78fb4088828c8
ZechariahWong <zechariahlives@gmail.com>
2021-01-31 15:33:45 +0800

    Cleaned up the user.h file a bit and added some functions in the .cpp

--- GameSocialPlatform_DSAAssignment/user.h
+++ GameSocialPlatform_DSAAssignment/user.h
@@ -2,35 +2,55 @@
 #include "userBinarySearchTree.h"
 #include "gameBinarySearchTree.h"
 #include "userMessageRecords.h"
+#include "Game.h"
+#include <list>
+using namespace std;
+
 
 class User
 {
 private:
-	std::string name;
-	std::string bio;
-	double accountBalance;
+	string bio;
+	string name;
+	bool online;					//true - online, false - offline
+	string password;
+	float accountBalance;
+
 	userBinarySearchTree friendsList;
-	userBinarySearchTree userInvitations; //accept/decline friend request
+	userBinarySearchTree userInvitations;	//accept/decline friend request
 	gameBinarySearchTree gameLibrary;
 
 	// this will be a dictionary, with the key being another user, and the value being a message record (to, from, message)
 	// I'll let you handle these zech, can refer to dictionary practical, stacks can be used to undo message
 	userMessageRecords records;
 
-	bool online; //true - online, false - offline
 
 public:
 	User();
-	void purchaseGame(); //add a game into the game library, unless the game already exists in the library
-	void playGame(); //play a game to notify friends who are online
-	void browseGameLibrary();
-	void browseFriends(); //can add the following options: online friends only/all friends
+	User (string n, string b, string p);
+
 	void editBio();
-	void messageFriend(); //creates a message 'record' between 1 user and another, only from friends in friends list
-	void topUpBalance(double amt);
-	void addFriend(User u); //add invitation to the other user's list
-	void acceptFriend(); //add both users into respective friends list, delete invitation
-	void declineFriend(); //delete invitation
+	void viewProfile ();
+	
+	// Basic getters
+	float getBalance ();
+	string getName ();
+	string getBio ();
+
+	//Game related 
+	void purchaseGame(Game g);		//add a game into the game library, unless the game already exists in the library
+	void playGame();				//play a game to notify friends who are online
+	void browseGameLibrary();
+	
+	// Balance related
+	void topUpBalance(float amt);
+
+	// Friend related
+	void acceptFriend();			//add both users into respective friends list, delete invitation
+	void messageFriend();			//creates a message 'record' between 1 user and another, only from friends in friends list
+	void browseFriends();			//can add the following options: online friends only/all friends
+	void declineFriend();			//delete invitation
+	void addFriend(User u);			//add invitation to the other user's list
 };
 
 #pragma once
